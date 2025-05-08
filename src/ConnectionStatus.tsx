edf163df8ed9cf9135ba90daf319733fdd96d346
Ankit Thakur <ankitthakur084@gmail.com>
2025-05-08 12:45:08 +0530

    removed connection status

--- src/ConnectionStatus.tsx
+++ src/ConnectionStatus.tsx
@@ -1,4 +1,4 @@
-import { HStack, Icon, Text } from "@chakra-ui/react";
+import { Icon } from "@chakra-ui/react";
 import { VscCircleFilled } from "react-icons/vsc";
 
 type ConnectionStatusProps = {
@@ -14,38 +14,13 @@ function ConnectionStatus({ connection, darkMode, isCollapsed = false }: Connect
     desynchronized: "red.500",
   }[connection];
 
-  const statusText = {
-    connected: "You are connected!",
-    disconnected: "Connecting to the server...",
-    desynchronized: "Disconnected, please refresh.",
-  }[connection];
-
-  if (isCollapsed) {
-    return (
-      <Icon
-        as={VscCircleFilled}
-        color={statusColor}
-        boxSize={4}
-        data-connection-status="true"
-      />
-    );
-  }
-
   return (
-    <HStack spacing={1}>
-      <Icon
-        as={VscCircleFilled}
-        color={statusColor}
-        data-connection-status="true"
-      />
-      <Text
-        fontSize="sm"
-        fontStyle="italic"
-        color={darkMode ? "gray.300" : "gray.600"}
-      >
-        {statusText}
-      </Text>
-    </HStack>
+    <Icon
+      as={VscCircleFilled}
+      color={statusColor}
+      boxSize={isCollapsed ? 4 : 3}
+      data-connection-status="true"
+    />
   );
 }
 
